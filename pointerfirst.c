52ad910ec02fae342c9ff9ced5083c78c10f29cb
Abdullah-0110 <abdullahdev88@gmail.com>
2023-12-21 23:55:50 +0500

    print values and sizes

--- pointerfirst.c
+++ pointerfirst.c
@@ -7,8 +7,10 @@ int main(void){
     int *ptr=&num;//declaring pointer and asign the adress of the num variable;
 
     //try to print the pointer 
-
-    printf("%d", *ptr);
+    printf("Num is= %d\n", num);//check the value of num
+    printf("Size of num=%d\n", sizeof(num));//check the size of num
+    printf("%d", *ptr);//print pointer value 
+    printf("Size of pointer=%d\n", ptr);//check the size of ptr or pointer
 
     return 0;
 
